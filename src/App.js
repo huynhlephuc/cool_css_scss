3a413d954106bbfdd15673a0a239c50194508f18
huynhlephuc <92335372+huynhlephuc@users.noreply.github.com>
2022-09-29 00:06:34 +0700

    lag text css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import './assets/index.scss';
 // import { SlideCheckbox } from './components/checkbox/slide_checkbox';
 // import { ChangeFontColor } from './components/font/change_font_color';
-import { SearchResponsive } from './components/search/search_responsive';
+// import { SearchResponsive } from './components/search/search_responsive';
+import { LagText } from './components/font/lag_text';
  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchResponsive />
+        <LagText />
       </header>
     </div>
   );
---
+++ src/components/font/lag_text.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LagText = () => {
+  return (
+    <div className="text" data-text="Hello World" >Hello World</div>
+  )
+}
