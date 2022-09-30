639339000702950f5272b737d4dcded6f51d49e1
huynhlephuc <92335372+huynhlephuc@users.noreply.github.com>
2022-09-30 22:29:33 +0700

    cut font

---
+++ src/components/font/font_cut.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FontCut = () => {
+  return (
+    <div style={{backgroundColor: 'black', width: '100%', height: '100vh'}}>
+      <div className="item" data-attr="HELLO WORLD">
+        HELLO WORLD
+        <div className="effect">
+            <div className="content">HELLO WORLD</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FontCut;
