f36d46220fad3ff736c0e18721f729bd7710b2ac
huynhlephuc <92335372+huynhlephuc@users.noreply.github.com>
2022-10-21 00:09:32 +0700

    finish letter

--- src/components/letter/show_letter.jsx
+++ src/components/letter/show_letter.jsx
@@ -6,7 +6,12 @@ const ShowLetter = () => {
         <div className="box">
             <input type="checkbox" id="message"></input>
             <div className="content">
-                1
+                <img alt="" src="https://anhdephd.vn/wp-content/uploads/2022/06/anh-cho-cute-anh-cho-chibi-3.jpg" />
+                <div className="title">Phúc Huỳnh</div>
+                <div className="des">
+                  Đây là nội dung của lá thư đó nha
+                </div>
+                <label className="" htmlFor="message">Close</label>
             </div>
             <label htmlFor="message" className="designBox"></label>
         </div>
